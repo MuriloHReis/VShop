79fcca583f05c774bb7529f6f5b0a780499db60f
Murilo Henrique <murilo.henrique@openlinesolutions.com.br>
2023-07-12 18:52:37 -0300

    Add project files.

---
+++ VShop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VShop.Web.Models;
+
+public class ProductViewModel
+{
+    public int Id { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public decimal Price { get; set; }
+    [Required]
+    public string? Description { get; set; }
+    [Required]
+    public long Stock { get; set; }
+    [Required]
+    public string? ImageURL { get; set; }
+    public string? CategoryName { get; set; }
+
+    [Range(1, 100)]
+    public int Quantity { get; set; } = 1;
+
+    [Display(Name ="Categorias")]
+    public int CategoryId { get; set; }
+}
